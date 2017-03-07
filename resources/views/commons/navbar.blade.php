64ae44280e7c42036135fb7cb7cab6c08e8186ce
Hitoshi Noujima <h.noujima@gmail.com>
2017-03-07 14:26:58 +0000

    user pages

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -16,7 +16,7 @@
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">プロフィール</a></li>
+                                <li>{!! link_to_route('users.show', 'プロフィール', ['id' => Auth::user()->id]) !!}</li>
                                 <li role="separator" class="divider"></li>
                                 <li>{!! link_to_route('logout.get', 'ログアウト') !!}</li>
                             </ul>
