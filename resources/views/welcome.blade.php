47035f7affb76a28b8f7e42728970e85b35ef233
Hitoshi Noujima <h.noujima@gmail.com>
2017-03-07 12:04:05 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>ようこそ！！</h1>
+            {!! link_to_route('signup.get', '新規登録', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
